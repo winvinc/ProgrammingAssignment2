a835a83bc2f3012b08dd29cfd95f516ecca6a336
winvinc <winvinc@gmail.com>
2015-03-22 14:35:38 +0700

    Commit draft 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Compution Inverse of Metrix is a costly computation and Time Consumption 
+## This Function will use benefic of cachce to reduce computation repeatly 
+## by caching the inverse of a matrix rather than compute it repeatedly. 
+## There are two functions that used to cache the inverse of a matrix.
 
 ## Write a short comment describing this function
-
+ProgrammingAssignment2
 makeCacheMatrix <- function(x = matrix()) {
 
 }
