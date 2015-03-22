1f6191ca35965250d6348ccc206353ff6053555b
winvinc <winvinc@gmail.com>
2015-03-22 17:09:57 +0700

    Final Version of Programming Assignment 2
    Final Version of Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,44 @@
 ## by caching the inverse of a matrix rather than compute it repeatedly. 
 ## There are two functions that used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
-ProgrammingAssignment2
-makeCacheMatrix <- function(x = matrix()) {
 
+## makeCacheMatrix (x) Function
+## This function creates a special "matrix" object that can cache its inverse.
+## There have 4 subfunctions in this Function
+## 1. set(newMetrix) = set the Metrix value
+## 2. get() = get The Matrix value
+## 3. setinverse(newInverseMetrix) = set The Inverse Metrix Value
+## 4. getinverse() = get The Inverse Metrix Value
+makeCacheMatrix <- function(x = matrix()) {
+  inverseMetrix <- NULL
+  set <- function(newMetrix) {
+    x <<- newMetrix
+    inverseMetrix <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(newInverseMetrix) inverseMetrix <<- newInverseMetrix
+  getinverse <- function() inverseMetrix
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+##cacheSolve (X,...)
+## This function computes the inverse of the special Metrix
+## and cache the Inverse Metrix to X
+## Program is Work as follow step
+## 1. checks to see if the Inverse Metrix has already been calculated. 
+## 2.If Yes, it gets the Inverse Metrix from the cache and skips the computation. 
+## 3.Else , it calculates the Inverse Metrix of the Metrix
+## and sets the value of the Inverse Metrix in the cache via the setinverse function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseMetrix <- x$getinverse()
+  if(!is.null(inverseMetrix)) {
+    message("inverseMetrix is Calculated")
+    message("getting cached data.")
+    return(inverseMetrix)
+  }
+  ValueMetrix <- x$get()
+  inverseMetrix <- solve(ValueMetrix)
+  x$setinverse(inverseMetrix)
+  inverseMetrix
 }
